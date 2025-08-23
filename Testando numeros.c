240a8f98d44efb889264fe287445e5cbc3bd4d1e
matheushss123 <67160529+affectendsand192@users.noreply.github.com>
2025-08-22 21:32:50 -0300

    Add files via upload

---
+++ Testando numeros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+char nome[10]="matheus";
+int idade=20;
+float altura=1.68;
+double salario = 1200.000;
+char inicial='m';
+
+printf("nome : %s\n", nome);
+printf("minha idade : %d\n",idade);
+printf("altura: %.2f\n",altura);
+printf("inicial do nome : %c\n",inicial);
+printf("salario : %.4f\n",salario);
+
+return 0;
+
+
+
+}
